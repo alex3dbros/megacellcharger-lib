9a48706c27b2619ac23ca9398a6b39dcee3db941
Alex <info@3d-brothers.com>
2023-12-13 16:36:38 +0200

    Fast forward

--- examples/GetCellChemistry.py
+++ examples/GetCellChemistry.py
@@ -5,7 +5,7 @@ import msgpack
 # Initiate the megacell charger class with the ip of your device
 tester = MegacellCharger("192.168.1.192")
 
-# Get Chemistry
+# Get Chemistry for cell id
 data = {
     "CiD": 0
 }
@@ -13,9 +13,5 @@ data = {
 result = tester.get_cell_chemistry(data)
 print(result)
 
-# decoded_packet = cobs.decode(result)
-# print(f"Received: {decoded_packet}")
-
-unpacked_data = msgpack.unpackb(result)  # Remove last byte
+unpacked_data = msgpack.unpackb(result)
 print(unpacked_data)
-# packet = bytearray()
